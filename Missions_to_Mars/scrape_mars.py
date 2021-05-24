2d56894844fa2f01359c2272a4ad493b067a954b
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-05-24 18:02:08 -0400

    Missions_to_Mars/

---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,9 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    
