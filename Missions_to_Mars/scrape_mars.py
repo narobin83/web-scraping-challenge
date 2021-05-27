015923de942ce5a1687b86e4cd41749a6bade460
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-05-27 16:36:14 -0400

    Missions_to_Mars/

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -6,3 +6,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
+# NASA Mars News Site Web Scraper
+def mars_news(browser):
+    # Visit the NASA Mars News Site
+    url = "https://mars.nasa.gov/news/"
+    browser.visit(url)
+
+    # Get First List Item & Wait Half a Second If Not Immediately Present
+    browser.is_element_present_by_css("ul.item_list li.slide")
+
+    html = browser.html
+    news_soup = BeautifulSoup(html, "html.parser")
